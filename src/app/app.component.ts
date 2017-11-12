c5a50a44ff78510b35648d948e61db3ab4bd7896
Łukasz Kuczek <lkuczek@wp.pl>
2017-11-12 15:12:12 +0100

    Ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-main',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Dynamiczne wiązania i zdarzenia';
+
+  
+  constructor(){
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-
+import { PlaylistsComponent } from './playlists/playlists.component';
+import { ContentCardComponent } from './playlists/content-card.component';
+import { PlaylistFormComponent } from './playlists/playlist-form.component';
+import { PlaylistsListComponent } from './playlists/playlists-list.component';
+import { PlaylistDetailComponent } from './playlists/playlist-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlaylistsComponent,
+    ContentCardComponent,
+    PlaylistFormComponent,
+    PlaylistsListComponent,
+    PlaylistDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
